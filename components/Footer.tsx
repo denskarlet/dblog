6aacc29d3a9c4a20636c35542c7746a6a616ede8
denskarlet <den.skarlet@gmail.com>
2021-07-06 17:47:22 -0500

    add placeholder components

---
+++ components/Footer.tsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <div>Some NEW Amazing Footer</div>;
+};
+
+export default Footer;
---
+++ components/Header.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+
+const links = [
+  ['/', 'Home'],
+  ['/blog', 'Blog'],
+  ['/about', 'About me'],
+  ['/contact', 'Contact'],
+  ['/github', 'Github'],
+];
+
+const Header = () => {
+  return (
+    <div>
+      <ul>
+        {links.map(([url, display]) => (
+          <li key={url}>
+            <Link href={url}>
+              <a>{display}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
