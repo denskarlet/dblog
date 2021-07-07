ea1a41c5d2c513d71bc1449b20d43f44084bc42b
denskarlet <den.skarlet@gmail.com>
2021-07-06 23:25:06 -0500

    add basic header styling, wip

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
+import { useState } from 'react';
 import styles from './Header.module.css';
-const { linkList } = styles;
+const { linkList, linkWrapper, wrapper, header, hamburger } = styles;
 const links = [
   ['/', 'Home'],
   ['/posts', 'Blog'],
@@ -10,11 +11,20 @@ const links = [
 ];
 
 const Header = () => {
+  const [showHamburger, setShowHamburger] = useState(false);
   return (
-    <div>
+    <div className={wrapper}>
+      <button
+        className={hamburger}
+        onClick={() => setShowHamburger(!showHamburger)}
+      >
+        HAM
+      </button>
+      {showHamburger && <div>Ill be a hamburger</div>}
+      <div className={header}>Denys&apos; dev blog</div>
       <ul className={linkList}>
         {links.map(([url, display]) => (
-          <li key={url}>
+          <li className={linkWrapper} key={url}>
             <Link href={url}>
               <a>{display}</a>
             </Link>
